649ea9344d63a16931c68da8ae4360226a9d8b4c
Augusto Llopis <augustollopisar@gmail.com>
2025-08-09 10:48:25 +0100

    Projects terminada

--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,7 @@ const sass = gulpSass(dartSass)
 /* HTML: copiar todo lo .html desde src a dist */
 export function html() {
   return src('src/**/*.html')
-    .pipe(dest('docs'))
+    .pipe(dest('dist'))
 }
 
 /* CSS: compilar SCSS -> CSS minificado + sourcemap */
